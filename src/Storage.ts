564c4041dddb5a3ba5354972a1516c1d297110f8
ryohidaka <39184410+ryohidaka@users.noreply.github.com>
2024-02-01 13:20:50 +0900

    feat: Add constant to wrap chrome.storage.sync.

--- src/Storage.ts
+++ src/Storage.ts
@@ -24,4 +24,24 @@ export const Storage = {
     set: async (items: SetItems, callback?: () => void) =>
       setter(StorageArea.Local, items, callback),
   },
+
+  /**
+   * Sync storage area.
+   */
+  Sync: {
+    /**
+     * Retrieves the value of the specified keys from the sync storage area.
+     * @param keys The keys of the items to be retrieved.
+     * @returns A promise that resolves with the values of the keys.
+     */
+    get: async (keys: StorageKeys) => getter(StorageArea.Sync, keys),
+
+    /**
+     * Sets the value of multiple items in the sync storage area.
+     * @param items An object containing one or more key-value pairs to be set.
+     * @param callback Optional callback function to be executed after setting the value.
+     */
+    set: async (items: SetItems, callback?: () => void) =>
+      setter(StorageArea.Sync, items, callback),
+  },
 };
